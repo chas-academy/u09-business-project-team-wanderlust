8d65a7139fe265be7a103239cca33054b1dc3ff6
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-05-20 15:55:19 +0200

    node-fetch types installed, restCountries.ts file created, initial fetching logic entered

---
+++ backend/src/api/restCountries.ts
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch';
+
+const API_URL = 'https://restcountries.com/v3.1';
+
+export const getAllCountries = async () => {
+    const response = await fetch(`${API_URL}/all`);
+    if (!response.ok) {
+        throw new Error('Hämtning av länder misslyckades');
+    }
+    return await response.json();
+};
+
+export const getCountryByName = async (name: string) => {
+    const response = await fetch(`${API_URL}/name/${name}`);
+    if (!response.ok) {
+        throw new Error(`Hämtning av ${name} misslyckades`);
+    }
+    return await response.json();
+
+}
