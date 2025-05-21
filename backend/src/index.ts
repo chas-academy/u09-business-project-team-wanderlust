b88a56c5ae64d1c656240b8579f3f389f73ef12c
Josefine klasson <jossanwowar93@gmail.com>
2025-05-21 15:33:22 +0200

    installed and added cors to backend now can get all countries

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,16 @@ import express from "express";
 import { connectDB } from './config/db';
 import countryRoutes from './routes/countries.routes'
 import userRoutes from './routes/user.routes';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 app.use('/api/countries', countryRoutes);
