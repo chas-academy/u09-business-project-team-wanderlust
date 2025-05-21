40254611f14e62b02629860eaa5542d827b1bc66
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-05-21 14:17:24 +0200

    created user service to allow adding to a list + compare countries

---
+++ backend/src/services/user.service.ts
@@ -0,0 +1,34 @@
+import User from '../models/user.model';
+import { getCountryByName } from './countries.service';
+
+export const addToFavorites = async (userId: string, code: string) => {
+    return await User.findByIdAndUpdate(userId, {
+        $addToSet: { favoriteCountries: code }
+    }, { new: true });
+};
+
+export const addToTravels = async (userId: string, code: string) => {
+    return await User.findByIdAndUpdate(userId, {
+        $addToSet: { travelPlans: code }
+}, { new: true });
+};
+
+export const compareCountries = async (name1: string, name2: string) => {
+    const [c1, c2] = await Promise.all([
+        getCountryByName(name1),
+        getCountryByName(name2)
+    ]);
+
+    const country1 = c1[0];
+    const country2 = c2[0];
+
+    return {
+        countries: [country1.name.common, country2.name.common],
+        region: [country1.region, country2.region],
+        population: [country1.name.population, country2.name.population],
+        currencies: [
+            Object.keys(country1.currencies || {})[0],
+            Object.keys(country2.currencies || {})[0],
+        ]
+    }
+}
