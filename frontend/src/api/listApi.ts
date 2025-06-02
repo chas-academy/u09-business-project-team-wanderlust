c3a1a3a83267c2aedf2f6aaf91491c090ffb89b7
Josefine Klasson <jossanwowar93@gmail.com>
2025-06-02 14:08:24 +0200

    removed old commeted out code and added env.example

--- frontend/src/api/listApi.ts
+++ frontend/src/api/listApi.ts
@@ -1,28 +1,4 @@
 import axios from "axios";
-/*
-const API_URL = "http://localhost:3000";
-
-export const fetchUserListWithDetails = async (userId: string, type: "favorites" | "travels") => {
-    const response = await axios.get(`${API_URL}/lists/details/${userId}/${type}`);
-    return response.data;
-};
-
-export const addCountryToList = async (
-    userId: string,
-    type: "favorites" | "travels",
-    code: string
-) => {
-    const response = await axios.put(
-        `${API_URL}/add/${userId}/${type}`,
-        { code }
-    );
-    return response.data;
-}
-
-export const removeCountryFromList = async (userId: string, type: "favorites" | "travels", code: string) => {
-    const response = await axios.put(`${API_URL}/remove/${userId}/${type}`, { code });
-    return response.data;
-}; */
 
 const API_URL = "http://localhost:3000/api";
 
