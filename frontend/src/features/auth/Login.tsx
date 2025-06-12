86d08d7dc6ad55dfae7df48bd9311c3350a1df44
Josefine Klasson <jossanwowar93@gmail.com>
2025-06-12 18:33:13 +0200

    made login in page looking a little better

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+// import { Header } from "../components/Header";
+// import LoginPage from "../../pages/loginPage";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
 
@@ -23,16 +25,46 @@ const Login = () => {
   }
 
   return (
-    <div className="login">
-      <h1>Logga in</h1>
-        <button onClick={signIn}>Login with Google</button>
+    
+<div className="flex items-center justify-center min-h-screen bg-gray-700">
+  <div
+    style={{
+      backgroundColor: '#f3f4f6',
+      width: '200px',
+      height: '200px',
+      padding: '1rem',
+      borderRadius: '0.5rem',
+      border: '1px solidrgb(5, 5, 5)', // Tailwind border-gray-600
+      textAlign: 'center',
+    }}
+  >
+    <h1 style={{ fontSize: '1.5rem', fontWeight: 600, marginBottom: '1rem' }}>
+      Logga in
+    </h1>
+
+    <button
+      style={{
+        backgroundColor: '#3b82f6 ',
+        color: 'white',
+        padding: '0.5rem 1.5rem',
+        borderRadius: '0.375rem',
+        cursor: 'pointer',
+        transition: 'background-color 0.2s',
+      }}
+        onClick={signIn}
+      >
+        Login with Google
+      </button>
             {/* Visa om användaren är inloggad */}
     {user ? (
       <p>Inloggad som: {user.username || user.email}</p>
     ) : (
-      <p>Inte inloggad</p>
+      <p>Inte inloggad, logga in genom att klicka på knappen ovan</p>
     )}
+    
     </div>
+    </div>
+    
   )
 };
 
